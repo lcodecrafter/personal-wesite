142f3865d4e94df553c1d43f00136fe0b93a124d
lCodecrafter <lcodecrafter@gmail.com>
2023-02-21 11:29:57 +0100

    Fix icons margins.

--- src/components/UI/icons/Codewars.tsx
+++ src/components/UI/icons/Codewars.tsx
@@ -11,8 +11,6 @@ export default function Codewars() {
       <svg
         className={styles.codewars}
         fill="#000000"
-        width="800px"
-        height="800px"
         viewBox="0 0 24 24"
         role="img"
         xmlns="http://www.w3.org/2000/svg"
