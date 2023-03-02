176adaf8e5e422ad3718be19d9a4b631b5bb95a2
lCodecrafter <lcodecrafter@gmail.com>
2023-03-02 13:16:34 +0100

    Add line width on prettier and a new page for future projects.

---
+++ src/pages/Experiments.tsx
@@ -0,0 +1,7 @@
+/** On this page i will show like mini experiments 
+like a project build on rust that do something, build an scraper,
+test a library etc. Single repo for all the experiments or differents
+repos??.
+Show experiments in cards */
+
+export default function Experiments() {}
